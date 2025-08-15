7775f539e20e4ab721c127fd50539acd7750d54e
Lydia Kebede <lydiakebede546@gmail.com>
2025-08-15 12:34:00 +0300

    Update one.py

--- one.py
+++ one.py
@@ -2,3 +2,70 @@
 print('Hello, World!')
 n = int(input().strip())
 #  strip will help us to remove any leading and trailing whitespace
+
+# Variables and basic maths 
+
+x = 10
+y = 5
+
+# basic maths operation 
+addition = x + y
+difference = x - y
+product = x * y
+quotient = x / y
+floor_div = x // y
+remainder = x % y 
+power = x **2
+
+print(addition)
+print(difference)
+a = 8
+b = 3
+
+print(a**2)
+print(b**2)
+print(a % b)
+
+#  Exercises 
+ 
+C = float(input('Enter the value of the Celsius' ))
+F = (C *9/5) + 32
+print(F)
+L = int(input('Enter the length of the rectangle '))
+W = int(input('Enter the width of the rectangle'))
+A = L * W 
+print("Area of rectangle is", A)
+P = 2 * (L  + W )
+print('Parameter of rectangle is', P)
+
+f = int(input('Enter the first number'))
+s = int(input('Enter the secnd number'))
+
+Add = f + s
+sub = f - s
+multi = f * s
+div = f / s
+print('their sum is equal to ', Add)
+print('their subtraction is equal to ', sub)
+print('their multiplication is equal to ', multi)
+print('their divison is equal to ', div)
+
+# Age Calculator 
+year = int(input("Enter your birth year"))
+currentYear = int(input('Enter the current year'))
+age = currentYear - year
+print(age)
+
+# Shopping Cart tool
+
+tshirt = float(input('Enter the price of the T-shirt'))
+dress = float(input('Enter the price of the dress'))
+trouser = float(input('Enter the price of the trouser'))
+total = tshirt + dress + trouser
+
+print(total)
+
+# Salary Calculator 
+
+
+
