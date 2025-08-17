3eec285c21fbf6ce429cc3d27ab35f268cc8179b
Lydia Kebede <lydiakebede546@gmail.com>
2025-08-17 19:53:08 +0300

    Update three.py

--- three.py
+++ three.py
@@ -13,3 +13,51 @@ print("Division:", a / b)
 print("Floor Division:", a // b)
 print("Modulus:", a % b)
 print("Exponent:", a ** b)
+#List and tuples 
+#List can be changed.
+my_list = [1,2,3,4,5]
+other_list = [8,4,9,7,2]
+# To access element 
+print(my_list[3])
+# Modify element
+my_list[4] = 6
+print(my_list)
+# Append an element
+my_list.append(7)
+print(my_list)
+# insert element
+my_list.insert(1,20)
+print(my_list)
+# Remove element by value
+my_list.remove(20)
+print(my_list)
+# remove element by index
+my_list.pop
+print(my_list)
+# length of list
+print(len(my_list))
+# Concatenate list
+print(my_list + other_list)
+# repeat_list
+print(my_list * 2)
+# Slice a list 
+print(my_list[1:3])
+
+# Tuple Unpacking
+x, y, z = (1,2,4)
+# Exercice one 
+apple = ["apple","banana","chery"]
+apple[1] = "orange"
+print(apple)
+
+
+#   Exercise 2
+colors = ("red","green","blue")
+print(colors[0])
+print(colors[2])
+print(1,"Avocado")
+print(colors)
+new_fruits = colors + ("yellow")
+print(colors)
+
+
