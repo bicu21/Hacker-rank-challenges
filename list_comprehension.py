0321e6a367e78fe20e8251de15e2137c497725c1
Lydia Kebede <lydiakebede546@gmail.com>
2025-09-05 11:15:27 +0300

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -33,7 +33,37 @@ for i in range(0, x+1):
                   if i + j + k != n:
                         print(i,j,k)
 
-                        
+# List comprehension in other day practice
+# In Python list is built in data type
+"""
+Features of list
+Oredered items = items have defined order.
+mutable = it can be changed added or removed
+allow dublicates 
+It can contain mixed data types
+
+Accessing element in list .
+By index 
+example fruits = ["apple","banana", "orange"]
+print(fruits([0]) this will give apple.
+negative indexing
+print([-1]) this will give orange 
+Slicing 
+example numbe = [1,2,3,4,5,6]
+print(number[1:4])
+this will give 2,3,4
+
+Common list operations 
+ a = []
+ a.append(10)
+ a.insert(0,5)  0 is index value = 5
+ a.extend ([15,20]) values
+ a.clear() this will remove all the values.
+ 
+"""
                  
+
+
+
 
 
